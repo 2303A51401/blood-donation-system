05751b478190ccb271065a0894a86610e9734f6c
soukya <2303a51401@sru.edu.in>
2026-06-30 20:26:29 +0530

    railway mysql update

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, redirect
 from database import mysql
+import os
 
 
 app = Flask(__name__)
 
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '@CHINNY#'
-app.config['MYSQL_DB'] = 'blood_bank'
+# Railway MySQL Connection
+
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_PORT'] = os.environ.get('MYSQL_PORT')
 
 
 mysql.init_app(app)
@@ -20,6 +24,8 @@ def home():
     return render_template("index.html")
 
 
+
+# REGISTER
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -34,12 +40,15 @@ def register():
 
         cur = mysql.connection.cursor()
 
+
         cur.execute(
             "INSERT INTO users(name,email,password,role) VALUES(%s,%s,%s,%s)",
             (name,email,password,role)
         )
 
+
         mysql.connection.commit()
+
 
         return redirect('/login')
 
@@ -48,12 +57,16 @@ def register():
 
 
 
+
+
 # LOGIN
 
 @app.route('/login', methods=['GET','POST'])
 def login():
 
+
     if request.method == "POST":
+
 
         email = request.form['email']
         password = request.form['password']
@@ -71,15 +84,22 @@ def login():
         user = cur.fetchone()
 
 
+
         if user:
+
             return redirect('/dashboard')
 
 
         else:
+
             return "Invalid Email or Password"
 
 
+
     return render_template("login.html")
+
+
+
 
 
 
@@ -92,6 +112,8 @@ def dashboard():
 
 
 
+
+
 # ADMIN
 
 @app.route('/admin')
@@ -101,12 +123,18 @@ def admin():
 
 
 
-# DONATE BLOOD + STOCK UPDATE
+
+
+
+
+# DONATE BLOOD
 
 @app.route('/donate', methods=['GET','POST'])
 def donate():
 
+
     if request.method == "POST":
+
 
         name = request.form['name']
         blood_group = request.form['blood_group']
@@ -114,7 +142,9 @@ def donate():
         phone = request.form['phone']
 
 
+
         cur = mysql.connection.cursor()
+
 
 
         cur.execute(
@@ -122,6 +152,9 @@ def donate():
         (name,blood_group,age,phone)
         )
 
+
+
+        # update stock
 
         cur.execute(
         "UPDATE blood_stock SET units = units + 1 WHERE blood_group=%s",
@@ -132,19 +165,29 @@ def donate():
         mysql.connection.commit()
 
 
+
         return "Donation Submitted Successfully"
+
 
 
     return render_template("donate.html")
 
 
 
+
+
+
+
+
 # REQUEST BLOOD
+
 
 @app.route('/request', methods=['GET','POST'])
 def blood_request():
 
+
     if request.method == "POST":
+
 
         name = request.form['name']
         blood_group = request.form['blood_group']
@@ -152,7 +195,9 @@ def blood_request():
         phone = request.form['phone']
 
 
+
         cur = mysql.connection.cursor()
+
 
 
         cur.execute(
@@ -161,52 +206,86 @@ def blood_request():
         )
 
 
+
         mysql.connection.commit()
 
 
+
         return "Blood Request Submitted Successfully"
+
 
 
     return render_template("request.html")
 
 
 
+
+
+
+
+
 # VIEW DONORS
+
 
 @app.route('/donors')
 def donors():
 
+
     cur = mysql.connection.cursor()
+
 
     cur.execute("SELECT * FROM donations")
 
+
     donors = cur.fetchall()
+
+
 
     return render_template("donors.html", donors=donors)
 
 
 
+
+
+
+
+
+
 # VIEW REQUESTS
+
 
 @app.route('/requests')
 def view_requests():
 
+
     cur = mysql.connection.cursor()
+
 
     cur.execute("SELECT * FROM requests")
 
+
     requests = cur.fetchall()
+
+
 
     return render_template("requests.html", requests=requests)
 
 
 
-# APPROVE REQUEST + STOCK DECREASE
+
+
+
+
+
+# APPROVE REQUEST
+
 
 @app.route('/approve/<int:id>')
 def approve(id):
 
+
     cur = mysql.connection.cursor()
+
 
 
     cur.execute(
@@ -214,13 +293,17 @@ def approve(id):
         (id,)
     )
 
+
     data = cur.fetchone()
+
 
 
     if data:
 
+
         blood_group = data[0]
         units = data[1]
+
 
 
         cur.execute(
@@ -229,52 +312,83 @@ def approve(id):
         )
 
 
+
         cur.execute(
         "UPDATE requests SET status=%s WHERE id=%s",
         ("Approved",id)
         )
 
 
+
         mysql.connection.commit()
+
 
 
     return redirect('/requests')
 
 
 
+
+
+
+
+
 # REJECT REQUEST
+
 
 @app.route('/reject/<int:id>')
 def reject(id):
 
+
     cur = mysql.connection.cursor()
+
+
 
     cur.execute(
         "UPDATE requests SET status=%s WHERE id=%s",
         ("Rejected",id)
     )
 
+
     mysql.connection.commit()
+
+
 
     return redirect('/requests')
 
 
 
+
+
+
+
+
 # BLOOD STOCK
+
 
 @app.route('/stock')
 def stock():
 
+
     cur = mysql.connection.cursor()
+
 
     cur.execute("SELECT * FROM blood_stock")
 
+
     stock = cur.fetchall()
+
 
 
     return render_template("stock.html", stock=stock)
 
 
 
+
+
+
+
+
 if __name__ == "__main__":
+
     app.run(debug=True)
